564bdd7e239c65f25931f0860b35090d679aa0b8
Your Name <you@example.com>
2022-08-28 12:15:55 +0300

    Updated

--- assets/js/menu-app.js
+++ assets/js/menu-app.js
@@ -9,3 +9,13 @@ document.querySelector('.mainmenu_toggle').onclick = function(){
         sign.classList.add('none');
     }
 }
+
+function scrollIt(){
+    if(document.body.scrollTop>document.querySelector('.header').clientHeight+1||document.documentElement.scrollTop>document.querySelector('.header').clientHeight+1){
+        document.body.querySelector('#main').before(document.querySelector('.header'));
+    }else{
+        document.body.querySelector('#main').getElementsByTagName("*")[0].before(document.querySelector('.header'));
+    }
+}
+
+window.onscroll=function(){if(scroller){scrollIt()}};
